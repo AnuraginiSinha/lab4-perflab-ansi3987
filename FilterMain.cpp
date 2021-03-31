b259a7ff9bb3d95e2f7b29cadf245dfde24f4fc4
Anuragini Sinha <ansi3987@colorado.edu>
2021-03-31 08:48:37 +0000

    FilterMain
    done

--- FilterMain.cpp
+++ FilterMain.cpp
@@ -2,8 +2,8 @@
 #include "cs1300bmp.h"
 #include <iostream>
 #include <fstream>
-#include <stdlib.h>
 #include "Filter.h"
+#include <omp.h>
 
 using namespace std;
 
@@ -65,7 +65,7 @@ main(int argc, char **argv)
 
 }
 
-class Filter *
+struct Filter *
 readFilter(string filename)
 {
   ifstream input(filename.c_str());
@@ -79,9 +79,9 @@ readFilter(string filename)
     filter -> setDivisor(div);
     for (int i=0; i < size; i++) {
       for (int j=0; j < size; j++) {
-	int value;
-	input >> value;
-	filter -> set(i,j,value);
+        int value;
+        input >> value;
+        filter -> set(i,j,value);
       }
     }
     return filter;
@@ -93,46 +93,85 @@ readFilter(string filename)
 
 
 double
-applyFilter(class Filter *filter, cs1300bmp *input, cs1300bmp *output)
+applyFilter(struct Filter *filter, cs1300bmp *input, cs1300bmp *output)
 {
+    long long cycStart, cycStop;
+    cycStart = rdtscll();
 
-  long long cycStart, cycStop;
-
-  cycStart = rdtscll();
-
-  output -> width = input -> width;
-  output -> height = input -> height;
-
-
-  for(int col = 1; col < (input -> width) - 1; col = col + 1) {
-    for(int row = 1; row < (input -> height) - 1 ; row = row + 1) {
-      for(int plane = 0; plane < 3; plane++) {
-
-	output -> color[plane][row][col] = 0;
-
-	for (int j = 0; j < filter -> getSize(); j++) {
-	  for (int i = 0; i < filter -> getSize(); i++) {	
-	    output -> color[plane][row][col]
-	      = output -> color[plane][row][col]
-	      + (input -> color[plane][row + i - 1][col + j - 1] 
-		 * filter -> get(i, j) );
-	  }
-	}
-	
-	output -> color[plane][row][col] = 	
-	  output -> color[plane][row][col] / filter -> getDivisor();
-
-	if ( output -> color[plane][row][col]  < 0 ) {
-	  output -> color[plane][row][col] = 0;
-	}
-
-	if ( output -> color[plane][row][col]  > 255 ) { 
-	  output -> color[plane][row][col] = 255;
-	}
-      }
+    int columns = (input->width) -  1;
+    int rows = (input->height) - 1;
+    output -> width = columns + 1;
+    output -> height = rows + 1;
+    
+    int size = filter -> getSize();
+    int red1, red2, red3, green4, green5, green6, blue7, blue8, blue9 = 0;//this refers to the colors of the three planes intially at 0
+    float divisor = filter -> getDivisor();
+    int *FilterStart; //this array will be in charge or applying the filter to the array of pixels
+    
+    int i = 0;
+    FilterStart = &i;//this will get the acc value of the plane and multiply filter to it
+    
+    #pragma omp parallel for
+    for(int row = 1; row < rows; row++) {
+        for(int col = 1; col < columns; col++) {
+            red1 = 0;
+            red2 = 0;
+            red3 = 0;
+            green4 = 0;
+            green5 = 0;
+            green6 = 0;
+            blue7 = 0;
+            blue8 = 0;
+            blue9 = 0;
+            
+            red1 += (input->color[0][row+i-1][col-1]*FilterStart[i*size]);//the first plane will be intialized row col at 0
+            red2 += (input->color[0][row+i][col-1]*FilterStart[(i+1)*size]);
+            red3 += (input->color[0][row+i+1][col-1]*FilterStart[(i+2)*size]);
+            green4 += (input->color[1][row+i-1][col-1]*FilterStart[i*size]);//this will start adding the values of the filter and plane at [1][0],[1][1][1][2]
+            green5 += (input->color[1][row+i][col-1]*FilterStart[(i+1)*size]);
+            green6 += (input->color[1][row+i+1][col-1]*FilterStart[(i+2)*size]);
+            blue7 += (input->color[2][row+i-1][col-1]*FilterStart[i*size]);
+            blue8 += (input->color[2][row+i][col-1]*FilterStart[(i+1)*size]);
+            blue9 += (input->color[2][row+i+1][col-1]*FilterStart[(i+2)*size]);
+            //this will be in charge of the values at [2][0] [2][1] [2][2]      
+            red1 += (input->color[0][row+i-1][col]*FilterStart[i*size+1]);
+            red2 += (input->color[0][row+i][col]*FilterStart[(i+1)*size+1]);//eventually all of these addtions and multiplciations will be added to eachother for the total val
+            red3 += (input->color[0][row+i+1][col]*FilterStart[(i+2)*size+1]);
+            green4 += (input->color[1][row+i-1][col]*FilterStart[i*size+1]);
+            green5 += (input->color[1][row+i][col]*FilterStart[(i+1)*size+1]);
+            green6 += (input->color[1][row+i+1][col]*FilterStart[(i+2)*size+1]);
+            blue7 += (input->color[2][row+i-1][col]*FilterStart[i*size+1]);
+            blue8 += (input->color[2][row+i][col]*FilterStart[(i+1)*size+1]);
+            blue9 += (input->color[2][row+i+1][col]*FilterStart[(i+2)*size+1]);
+                    
+            red1 += (input->color[0][row+i-1][col+1]*FilterStart[i*size+2]);
+            red2 += (input->color[0][row+i][col+1]*FilterStart[(i+1)*size+2]);
+            red3 += (input->color[0][row+i+1][col+1]*FilterStart[(i+2)*size+2]);
+            green4 += (input->color[1][row+i-1][col+1]*FilterStart[i*size+2]);
+            green5 += (input->color[1][row+i][col+1]*FilterStart[(i+1)*size+2]);
+            green6 += (input->color[1][row+i+1][col+1]*FilterStart[(i+2)*size+2]);
+            blue7 += (input->color[2][row+i-1][col+1]*FilterStart[i*size+2]);
+            blue8 += (input->color[2][row+i][col+1]*FilterStart[(i+1)*size+2]);
+            blue9 += (input->color[2][row+i+1][col+1]*FilterStart[(i+2)*size+2]);             
+            
+            red1 = (red1+red2+red3) / divisor; // the divisor for each plane is 3 in total 9
+            green4 = (green4+green5+green6) / divisor;
+            blue7 = (blue7+blue8+blue9) / divisor;
+            
+            (red1 < 0) ? red1=0 : red1=red1; //ternaray will check if red1 val < 0 will be initalized at 0
+            (red1 > 255) ? red1=255 : red1=red1; //same with val> 255 make 255
+            
+            (green4 < 0) ? green4=0 : green4=green4;
+            (green4 > 255) ? green4=255 : green4 = green4;
+            
+            (blue7 < 0) ? blue7=0 : blue7=blue7;
+            (blue7 > 255) ? blue7=255 : blue7=blue7;
+            
+            output -> color[0][row][col] = red1; //filter is applied to all 9 pixels!
+            output -> color[1][row][col] = green4;
+            output -> color[2][row][col] = blue7;
+        }
     }
-  }
-
   cycStop = rdtscll();
   double diff = cycStop - cycStart;
   double diffPerPixel = diff / (output -> width * output -> height);
